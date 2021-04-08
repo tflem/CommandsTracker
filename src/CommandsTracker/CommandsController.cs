58e7f308578e06f34b6544841ff988477bd33f38
tfleming <tfleming26@hotmail.com>
2021-04-08 17:53:33 -0600

    Initial commit

---
+++ src/CommandsTracker/CommandsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandTracker.Controllers 
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase 
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() 
+        {
+            return new string[] { "this", "is", "a", "test", "!" };
+        }
+    }
+}
+
+
+
